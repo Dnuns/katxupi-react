80945377e78caeb41728566febbbc831cde904a9
David Nunes <51808846+Dnuns@users.noreply.github.com>
2022-05-03 00:40:50 -0100

    Update index.js

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Main = styled.main`
     max-height: 95%;
     max-width: 80%;
-    height:95%;
+    height:100%;
     width: 80%;
     display: flex;
     flex-direction: column;
